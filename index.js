5268c32829ee4729bfd6fd980df3aa59f2f5e9cd
aleksq7 <morrisalex2001@gmail.com>
2021-08-13 11:02:49 -0600

    commit tree b1394f6b600c25a786e679a396a41d64f1dba442

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(array, name){
+    const matchingNames = array.filter(function(item){
+        if (item === item.toLowerCase()){
+            return item === name.toLowerCase();
+        } else {
+            return item === name;
+        }
+    })
+    return matchingNames;
+}
+
+function fuzzyMatch(array, string){
+    const matchingNames = array.filter(function(item){
+        return item.slice(0, string.length) === string})
+    return matchingNames;
+}
+
+function matchName(array, string){
+    const matchingDrivers = array.filter(function(item){
+        return item.name === string;
+    })
+    return matchingDrivers;
+}
